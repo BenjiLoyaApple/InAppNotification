3ffe9e271504cdebe58f37f0765f776ab0f6ee6a
Benji Loya <113779387+BenjiLoyaApple@users.noreply.github.com>
2024-09-13 16:39:14 +0500

    Initial Commit

---
+++ InAppNotification/InAppNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  InAppNotificationApp.swift
+//  InAppNotification
+//
+//  Created by Benji Loya on 13.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct InAppNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
